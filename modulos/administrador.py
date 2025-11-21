93ae55e9829aa6759d8d9c59966bc07ffeea302a
PLopez1997 <00001922@uca.edu.sv>
2025-11-20 21:52:14 -0600

    Update administrador.py

--- modulos/administrador.py
+++ modulos/administrador.py
@@ -1,5 +1,8 @@
-import streamlit as st
-
-def administrador_page():
-    st.title("Entorno administrador")
-    st.write("Contenido exclusivo para el administrador.")
+# Mostrar el menú lateral
+opciones = ["Grupos", "Otra opción"] # Agrega más opciones si las necesitas
+seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+# Según la opción seleccionada, mostramos el contenido correspondiente
+if seleccion == "Grupos":
+    administrador_page()
+elif seleccion == "Otra opción":
+    st.write("Has seleccionado otra opción.") # Aquí podrías agregar el contenido de otras opciones
