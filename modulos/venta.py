546e2d7212e97ad5da8f8633f49ac356052bba74
PLopez1997 <00001922@uca.edu.sv>
2025-11-20 18:52:16 -0600

    Update venta.py

--- modulos/venta.py
+++ modulos/venta.py
@@ -2,5 +2,5 @@
 import streamlit as st
 def mostrar_venta():
  # Aquí se muestra "Hola Mundo"
- st.title("Hola Mundo")
- st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
+ st.title("Administrador")
+ st.write("Este es el entorno del administrador")
